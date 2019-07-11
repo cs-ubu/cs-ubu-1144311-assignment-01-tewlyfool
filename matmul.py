a4b6f6f8479298797e6826f6276281c9ce1539b2
tewlyfool <pongsaton.ng.58@ubu.ac.th>
2019-07-11 12:16:43 +0700

    11/7/2562

---
+++ matmul.py
@@ -0,0 +1,19 @@
+
+def readm(a):
+    a =open('b.csv','r')
+    b =[]
+    for i in a.readlines():
+        b.append([float(x) for x  in i.strip().split(',')])
+    a.close()
+    return b
+def matmul(A,b):
+    m,n = len(A),len(b[0])
+
+    if len(A[0])==len(b):
+        C = [ [0]*n]*m
+        for r in range(m):
+            for c in range(n):
+                C[r][c]=sum(A[r][j]*b[j][c] for j in  range(len(A[0])))
+        return C
+    else : 
+        return []
---
+++ numpyla.py
@@ -0,0 +1,10 @@
+import numpy as np
+import numpy.linalg as linalg
+A = np.array( [
+    [ 4.0, -2.0,  1.0],
+    [-2.0,  4.0, -2.0],
+    [ 1.0, -2.0,  3.0] ] )
+b = np.array( [1.0, 4.0, 2.0] )
+linalg.inv(A)
+linalg.det(A)
+linalg.solve(A, b)
